d03173bf5821b7400f0a9bc0dc402fe16cd2058f
Dion Almaer <dion@almaer.com>
2010-03-08 22:49:34 -0800

    use Mojo.Log because of console.log bug

--- app/assistants/first-assistant.js
+++ app/assistants/first-assistant.js
@@ -2,7 +2,7 @@ function FirstAssistant() {}
 
 FirstAssistant.prototype = {    
     setup: function() {
-        console.log("setup called! you see this if debugging is on nicely for you");
+        Mojo.Log.info("setup called! you see this if debugging is on nicely for you");
 
     	this.controller.get("app-title").update(Mojo.appInfo.title);
     	this.controller.get("app-id").update(Mojo.appInfo.id);
